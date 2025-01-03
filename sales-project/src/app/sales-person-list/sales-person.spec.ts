42c3bbcf8ef88fbfc7cbf018aa0a5872c0831a7f
FilmonGhirmatzion <FilmonGhirmatzion@gmail.com>
2025-01-03 11:47:08 -0600

    person class and list added

---
+++ sales-project/src/app/sales-person-list/sales-person.spec.ts
@@ -0,0 +1,7 @@
+import { SalesPerson } from '../sales_person-list/sales-person';
+
+describe('SalesPerson', () => {
+  it('should create an instance', () => {
+    expect(new SalesPerson()).toBeTruthy();
+  });
+});
